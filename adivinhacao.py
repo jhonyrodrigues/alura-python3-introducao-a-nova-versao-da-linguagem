f75a47b772762552b5536f4d3bcc1708878c47eb
Jhony de Oliveira Rodrigues <jhony.o_rodrigues@hotmail.com>
2022-04-11 21:41:54 -0300

    lidando com a entrada do usuário

---
+++ adivinhacao.py
@@ -0,0 +1,18 @@
+print("************************************")
+print("* Bem vindo ao jogo de adivinhação *")
+print("************************************")
+
+numero_secreto = 42
+
+chute_str = input("Digite o seu número: ")
+
+chute = int(chute_str)
+
+print("Você digitou: ", chute)
+
+if numero_secreto == chute:
+    print("Você acertou !")
+else:
+    print("Você errou !")
+
+print("Fim do jogo ")
