2aeb103bfe261ec6e5b92c470ff3a5dcdceb4688
Jhony de Oliveira Rodrigues <jhony.o_rodrigues@hotmail.com>
2022-04-18 20:19:33 -0300

    implementando o encerramento do jogo

--- forca.py
+++ forca.py
@@ -3,22 +3,35 @@ def jogar():
     print("***  Bem vindo ao jogo de forca  ***")
     print("************************************")
 
-    palavra_secreta = "banana"
-    letras_acertadas = ["_", "_", "_", "_", "_", "_"]
+    palavra_secreta = "banana".upper()
+    letras_acertadas = ["_" for letra in palavra_secreta]
+
     enforcou = False
     acertou = False
+    erros = 0
 
     while not enforcou and not acertou:
 
-        chute = input("Qual letra? ").strip()
-        index = 0
+        chute = input("Qual letra? ")
+        chute = chute.strip().upper()
 
-        for letra in palavra_secreta:
-            if chute.upper() == letra.upper():
-                letras_acertadas[index] = letra
-            index = index + 1
+        if chute in palavra_secreta:
+            index = 0
+            for letra in palavra_secreta:
+                if chute == letra:
+                    letras_acertadas[index] = letra
+                index += 1
+        else:
+            erros += 1
+        enforcou = erros == 6
+        acertou = "_" not in letras_acertadas
         print(letras_acertadas)
 
+    if acertou:
+        print("Você acertou")
+    else:
+        print("Você perdeu")
+        
     print("Fim do jogo ")
 
 
